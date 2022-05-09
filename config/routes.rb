8b722f3d1e8ea332944978a88cc5c477419019f4
codewtt <tahar.tcontact@gmail.com>
2022-05-09 18:21:28 +0200

    to finish

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  root 'gossips#index'
+  get 'static_pages/welcome'
+  get 'static_pages/team'
+  get 'static_pages/contact'
+  get 'static_pages/welcome/(:first_name)', to: 'static_pages#welcome'
+
+  resources :gossips do
+    resources :comments
+    resources :favorites
+  end
+  resources :users
+  resources :cities
+  resources :sessions, only:[:new, :create, :destroy]
+end
